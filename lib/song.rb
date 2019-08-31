1c7ae71b5c9d807be6e254f1778c83e459039d71
github username <github email address>
2019-08-31 14:18:06 +0100

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
 
     attr_accessor :name, :artist, :genre
@@ -16,7 +17,51 @@ class Song
     
     end
     
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        genre_count_hash = {}
+        @@genres.each do |genre| 
+            if genre_count_hash[genre]
+               genre_count_hash[genre] += 1
+            else
+               genre_count_hash[genre] = 1
+            end
+        end
+        genre_count_hash
+    end 
+
+    def self.artist_count
+        artist_count_hash = {}
+        @@artists.each do |artist| 
+            if artist_count_hash[artist]
+               artist_count_hash[artist] += 1
+            else
+               artist_count_hash[artist] = 1
+            end
+        end
+        artist_count_hash
+    end 
+
+
 
       
 
 end
+
+tk = Song.new("TakeshiAwesome","Takeshi","Rock")
+ek = Song.new("EtsukoGreat","Etsuko","Enka")
+ek = Song.new("SusumuIkiru","Susumu","Enka")
+
+binding.pry
+"test"
