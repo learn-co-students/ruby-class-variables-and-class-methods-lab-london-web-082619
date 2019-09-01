e8fce1dd1c7d0d699734b4abf4a653c0971493dc
github username <github email address>
2019-09-01 08:48:54 +0100

    Done.

--- test2.rb
+++ test2.rb
@@ -1,27 +1,36 @@
-class Song
-    
-    
-          
-    attr_accessor :name, :artist, :genre
-    
+require 'pry'
 
-    @@count = 0
-    @@artists = []
-    @@genres = []
+class Book
+  
+  attr_accessor :title, :genre
+  
+  GENRES = []
+  @@books = []
+
+  def initialize(title, genre)
+    @title = title
+    self.class.all << self #why only the title goes in? I guesses the genre also goes in.
+    GENRES << genre
     
-    def initialize(name, artist, genre)
-    @@count += 1
-    @@genres << genre
-    @@artists << artist
-    @name = name
-    @artist = artist
-    @genre = genre
-    
-    end
-    
+  end
+
+  def self.all
+    @@books
+     #GENRES
+  end
+
+
+def show_all
+    puts @@books
+    puts GENRES
+  end
+  
 end
- 
-takeshi = Song.new("TakeshiAwesome", "Tak", "Rock")
-p takeshi.name
-p takeshi.artist
-p takeshi.genre
+
+takeshi = Book.new("Takeshi", "Comedy")
+etsu = Book.new("Etsu", "Hisrory")
+
+ takeshi.show_all
+
+ binding.pry
+ "test"
